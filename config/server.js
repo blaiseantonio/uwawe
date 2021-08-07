39af4853db3c5dd6885da92acdfe4a006575e653
Blaise Antonio I <antonio.iradukunda@gmail.com>
2021-08-07 04:25:11 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '530804366d3265ce7cbb9572346d4280'),
+    },
+  },
+});
